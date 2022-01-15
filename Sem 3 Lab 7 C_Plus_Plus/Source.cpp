19a7d9d5834501ef3b88f32f03cb7dd01acbe218
Black_Wolfy007 <shuvaev.v.6@gmail.com>
2022-01-16 02:07:25 +0700

    Список изменений: ---

--- Sem 3 Lab 7 C_Plus_Plus/Source.cpp
+++ Sem 3 Lab 7 C_Plus_Plus/Source.cpp
@@ -1,18 +1,87 @@
 #include <iostream>
 
 using namespace std;
-// Вариант №1 (бинарное дерево для предложения)
 
-class Binary_Tree {
+struct Node
+{
+	int x;
 
-public:
-	Binary_Tree(string sequence) {
-
-	}
-
+	Node* l, * r;
 };
 
-int main() {
-	
-	return 0;
+void show(Node*& Tree)
+{
+	if (Tree != NULL)
+	{
+		show(Tree->l);
+
+		cout << Tree->x << " ";
+
+		show(Tree->r);
+	}
+}
+
+void add_node(int x, Node*& tree)
+{
+	if (NULL == tree)
+	{
+		tree = new Node;
+
+		tree->x = x;
+
+		tree->l = tree->r = NULL;
+	}
+	if (x < tree->x)
+	{
+		if (tree->l != NULL)
+		{
+			add_node(x, tree->l);
+		}
+		else
+		{
+			tree->l = new Node;
+
+			tree->l->l = tree->l->r = NULL;
+
+			tree->l->x = x;
+		}
+	}
+	if (x > tree->x)
+	{
+		if (tree->r != NULL)
+		{
+			add_node(x, tree->r);
+		}
+		else
+		{
+			tree->r = new Node;
+
+			tree->r->l = tree->r->r = NULL;
+
+			tree->r->x = x;
+		}
+	}
+}
+
+int main()
+{
+	Node* Tree = NULL;
+
+	add_node(6, Tree);
+
+	add_node(4, Tree);
+
+	add_node(6, Tree);
+
+	add_node(1, Tree);
+
+	add_node(0, Tree);
+
+	add_node(3, Tree);
+
+	add_node(-9, Tree);
+
+	add_node(5, Tree);
+
+	show(Tree);
 }
