3880ede27f040c04448d1dd9eba6508c01f3e05a
Black_Wolfy007 <shuvaev.v.6@gmail.com>
2022-01-16 16:30:39 +0700

    Список изменений: Переписан код на класс Tree_Sort, работающий с массивом чисел; Добавлена диаграмма класса

--- Sem 3 Lab 7 C_Plus_Plus/Source.cpp
+++ Sem 3 Lab 7 C_Plus_Plus/Source.cpp
@@ -1,87 +1,147 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <Windows.h>
 
 using namespace std;
 
-struct Node
-{
-	int x;
+const string Out_F = "Result.txt";//	Константа типа string, содержащая в себе имя файла вывода результатов работы дерева
 
-	Node* l, * r;
+class Tree_Sort{
+private:
+	vector<int> tree;//	Переменная, содержащая в себе бинарное дерево
+	int level_counter;//Переменная, содержащая в себе количество уровней дерева
+
+public:
+	// Конструктор класса по-умолчанию, запускающий ввод массива чисел для сортировки
+	Tree_Sort() {
+		tree.push_back(NULL);
+		level_counter = 1;
+		Tree_Assign();
+	}
+	//	Конструктор класса, принимающий динамический массив чисел для сортировки
+	Tree_Sort(vector<int> temp) {
+		tree.clear();
+		level_counter = 0;
+		if (temp.size()) {
+			Tree_Level_Append();
+			tree[0] = temp[0];
+
+			for (int i = 1; i < temp.size(); i++) {
+				Num_Sort(temp[i], 0);
+			}
+		}
+		else {
+			cout << "Принятый массив невозможно обработать, т.к. он является пустым." << endl;
+		}
+
+	}
+
+	//	Метод, отвечающий за увеличение кол-ва уровней (а с ними - и кол-ва элементов)
+	void Tree_Level_Append() {
+		level_counter++;
+		int temp = pow(2, level_counter-1);
+		for (int i = 0; i < temp; i++)
+			tree.push_back(NULL);
+	}
+	//	Метод, отвечающий за распределение числа по дереву
+	void Num_Sort(int number, int current_index) {
+		
+		if (current_index >= tree.size()) {
+			Tree_Level_Append();
+		}
+		
+		if (tree[current_index] == NULL) {
+			tree[current_index] = number;
+		}
+		else if (number <= tree[current_index]) {
+			Num_Sort(number, current_index * 2 + 1);
+		}
+		else Num_Sort(number, current_index * 2 + 2);
+		
+	}
+	//	Метод, отвечающий за ввод сортируемых чисел относительно первого введенного
+	void Tree_Assign() {
+		tree.clear();
+		level_counter = 0;
+		vector<int> temp;
+		int temp_counter = 0, temp_num = 0;
+
+		cout << "Введите количество вводимых чисел:\n: ";
+		cin >> temp_counter;
+		cout << endl;
+
+		for (int i = 0; i < temp_counter; i++) {
+			cout << "Введите число №" << i+1 << ":\n: ";
+			cin >> temp_num;
+			temp.push_back(temp_num);
+			cout << endl;
+		}
+
+		Tree_Level_Append();
+		tree[0] = temp[0];
+
+		for (int i = 1; i < temp.size(); i++) {
+			Num_Sort(temp[i], 0);
+		}
+	}
+	//	Метод, отвечающий за конвертацию дерево в строку
+	string Tree_ToString() {
+		string output_string = "";
+
+		int zapolnitel_counter = 0, item_count = 0, current_level_num_count = 0;
+
+		for (int i = 0; i < level_counter; i++) {
+			zapolnitel_counter = pow(2, level_counter - i) - 1;
+			current_level_num_count = pow(2, i);
+
+			for (int j = 0; j < current_level_num_count; j++) {
+				for (int k = 0; k < zapolnitel_counter; k++) {
+					output_string += " ";
+				}
+
+				output_string += to_string(tree[item_count]);
+
+				for (int k = 0; k < zapolnitel_counter; k++) {
+					output_string += " ";
+				}
+				if (j < current_level_num_count - 1) output_string += " ";
+				item_count++;
+			}
+
+			output_string += "\n";
+		}
+		return output_string;
+	}
+	//	Метод, отвечающий за вывод в консоль
+	void Tree_Print() {
+		cout << Tree_ToString();
+	}
+	//	Метод, отвечающий за вывод в файл
+	void Tree_SaveToFile() {
+		
+		ofstream setdata(Out_F);
+
+		setdata << Tree_ToString();
+
+		setdata.close();
+	}
 };
 
-void show(Node*& Tree)
-{
-	if (Tree != NULL)
-	{
-		show(Tree->l);
+int main() {
+	setlocale(LC_ALL, "Rus");
 
-		cout << Tree->x << " ";
+	ofstream clearOut(Out_F);
+	clearOut.close();
 
-		show(Tree->r);
-	}
-}
+	//vector<int> Test_mass = {10,9,11,10};
+	//Tree_Sort test(Test_mass);
 
-void add_node(int x, Node*& tree)
-{
-	if (NULL == tree)
-	{
-		tree = new Node;
+	Tree_Sort test;
 
-		tree->x = x;
+	test.Tree_Print();
+	test.Tree_SaveToFile();
 
-		tree->l = tree->r = NULL;
-	}
-	if (x < tree->x)
-	{
-		if (tree->l != NULL)
-		{
-			add_node(x, tree->l);
-		}
-		else
-		{
-			tree->l = new Node;
-
-			tree->l->l = tree->l->r = NULL;
-
-			tree->l->x = x;
-		}
-	}
-	if (x > tree->x)
-	{
-		if (tree->r != NULL)
-		{
-			add_node(x, tree->r);
-		}
-		else
-		{
-			tree->r = new Node;
-
-			tree->r->l = tree->r->r = NULL;
-
-			tree->r->x = x;
-		}
-	}
-}
-
-int main()
-{
-	Node* Tree = NULL;
-
-	add_node(6, Tree);
-
-	add_node(4, Tree);
-
-	add_node(6, Tree);
-
-	add_node(1, Tree);
-
-	add_node(0, Tree);
-
-	add_node(3, Tree);
-
-	add_node(-9, Tree);
-
-	add_node(5, Tree);
-
-	show(Tree);
+	return 0;
 }
