0022ba0016b81d0e43989b21b8ac6550f087669b
Shuvaev_Vadim <shuvaev.v.6@gmail.com>
2022-08-26 05:18:13 +0700

    Some changes

--- Sem 3 Lab 7 C_Plus_Plus/Source.cpp
+++ Sem 3 Lab 7 C_Plus_Plus/Source.cpp
@@ -1,147 +1,142 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include <Windows.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-const string Out_F = "Result.txt";//	Константа типа string, содержащая в себе имя файла вывода результатов работы дерева
+const string Out_F = "Result.txt";  //	РљРѕРЅСЃС‚Р°РЅС‚Р° С‚РёРїР° string, СЃРѕРґРµСЂР¶Р°С‰Р°СЏ РІ СЃРµР±Рµ
+                                    //РёРјСЏ С„Р°Р№Р»Р° РІС‹РІРѕРґР° СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ СЂР°Р±РѕС‚С‹ РґРµСЂРµРІР°
 
-class Tree_Sort{
-private:
-	vector<int> tree;//	Переменная, содержащая в себе бинарное дерево
-	int level_counter;//Переменная, содержащая в себе количество уровней дерева
+class Tree_Sort {
+ private:
+  vector<int> tree;  //	РџРµСЂРµРјРµРЅРЅР°СЏ, СЃРѕРґРµСЂР¶Р°С‰Р°СЏ РІ СЃРµР±Рµ Р±РёРЅР°СЂРЅРѕРµ РґРµСЂРµРІРѕ
+  int level_counter;  //РџРµСЂРµРјРµРЅРЅР°СЏ, СЃРѕРґРµСЂР¶Р°С‰Р°СЏ РІ СЃРµР±Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ СѓСЂРѕРІРЅРµР№ РґРµСЂРµРІР°
 
-public:
-	// Конструктор класса по-умолчанию, запускающий ввод массива чисел для сортировки
-	Tree_Sort() {
-		tree.push_back(NULL);
-		level_counter = 1;
-		Tree_Assign();
-	}
-	//	Конструктор класса, принимающий динамический массив чисел для сортировки
-	Tree_Sort(vector<int> temp) {
-		tree.clear();
-		level_counter = 0;
-		if (temp.size()) {
-			Tree_Level_Append();
-			tree[0] = temp[0];
+ public:
+  // РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РєР»Р°СЃСЃР° РїРѕ-СѓРјРѕР»С‡Р°РЅРёСЋ, Р·Р°РїСѓСЃРєР°СЋС‰РёР№ РІРІРѕРґ РјР°СЃСЃРёРІР° С‡РёСЃРµР» РґР»СЏ
+  // СЃРѕСЂС‚РёСЂРѕРІРєРё
+  Tree_Sort() {
+    tree.push_back(NULL);
+    level_counter = 1;
+    Tree_Assign();
+  }
+  //	РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РєР»Р°СЃСЃР°, РїСЂРёРЅРёРјР°СЋС‰РёР№ РґРёРЅР°РјРёС‡РµСЃРєРёР№ РјР°СЃСЃРёРІ С‡РёСЃРµР» РґР»СЏ СЃРѕСЂС‚РёСЂРѕРІРєРё
+  Tree_Sort(vector<int> temp) {
+    tree.clear();
+    level_counter = 0;
+    if (temp.size()) {
+      Tree_Level_Append();
+      tree[0] = temp[0];
 
-			for (int i = 1; i < temp.size(); i++) {
-				Num_Sort(temp[i], 0);
-			}
-		}
-		else {
-			cout << "Принятый массив невозможно обработать, т.к. он является пустым." << endl;
-		}
+      for (int i = 1; i < temp.size(); i++) {
+        Num_Sort(temp[i], 0);
+      }
+    } else {
+      cout << "РџСЂРёРЅСЏС‚С‹Р№ РјР°СЃСЃРёРІ РЅРµРІРѕР·РјРѕР¶РЅРѕ РѕР±СЂР°Р±РѕС‚Р°С‚СЊ, С‚.Рє. РѕРЅ СЏРІР»СЏРµС‚СЃСЏ РїСѓСЃС‚С‹Рј."
+           << endl;
+    }
+  }
 
-	}
+  //	РњРµС‚РѕРґ, РѕС‚РІРµС‡Р°СЋС‰РёР№ Р·Р° СѓРІРµР»РёС‡РµРЅРёРµ РєРѕР»-РІР° СѓСЂРѕРІРЅРµР№ (Р° СЃ РЅРёРјРё - Рё РєРѕР»-РІР°
+  //СЌР»РµРјРµРЅС‚РѕРІ)
+  void Tree_Level_Append() {
+    level_counter++;
+    int temp = pow(2, level_counter - 1);
+    for (int i = 0; i < temp; i++) tree.push_back(NULL);
+  }
+  //	РњРµС‚РѕРґ, РѕС‚РІРµС‡Р°СЋС‰РёР№ Р·Р° СЂР°СЃРїСЂРµРґРµР»РµРЅРёРµ С‡РёСЃР»Р° РїРѕ РґРµСЂРµРІСѓ
+  void Num_Sort(int number, int current_index) {
+    if (current_index >= tree.size()) {
+      Tree_Level_Append();
+    }
 
-	//	Метод, отвечающий за увеличение кол-ва уровней (а с ними - и кол-ва элементов)
-	void Tree_Level_Append() {
-		level_counter++;
-		int temp = pow(2, level_counter-1);
-		for (int i = 0; i < temp; i++)
-			tree.push_back(NULL);
-	}
-	//	Метод, отвечающий за распределение числа по дереву
-	void Num_Sort(int number, int current_index) {
-		
-		if (current_index >= tree.size()) {
-			Tree_Level_Append();
-		}
-		
-		if (tree[current_index] == NULL) {
-			tree[current_index] = number;
-		}
-		else if (number <= tree[current_index]) {
-			Num_Sort(number, current_index * 2 + 1);
-		}
-		else Num_Sort(number, current_index * 2 + 2);
-		
-	}
-	//	Метод, отвечающий за ввод сортируемых чисел относительно первого введенного
-	void Tree_Assign() {
-		tree.clear();
-		level_counter = 0;
-		vector<int> temp;
-		int temp_counter = 0, temp_num = 0;
+    if (tree[current_index] == NULL) {
+      tree[current_index] = number;
+    } else if (number <= tree[current_index]) {
+      Num_Sort(number, current_index * 2 + 1);
+    } else
+      Num_Sort(number, current_index * 2 + 2);
+  }
+  //	РњРµС‚РѕРґ, РѕС‚РІРµС‡Р°СЋС‰РёР№ Р·Р° РІРІРѕРґ СЃРѕСЂС‚РёСЂСѓРµРјС‹С… С‡РёСЃРµР» РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ РїРµСЂРІРѕРіРѕ
+  //РІРІРµРґРµРЅРЅРѕРіРѕ
+  void Tree_Assign() {
+    tree.clear();
+    level_counter = 0;
+    vector<int> temp;
+    int temp_counter = 0, temp_num = 0;
 
-		cout << "Введите количество вводимых чисел:\n: ";
-		cin >> temp_counter;
-		cout << endl;
+    cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ РІРІРѕРґРёРјС‹С… С‡РёСЃРµР»:\n: ";
+    cin >> temp_counter;
+    cout << endl;
 
-		for (int i = 0; i < temp_counter; i++) {
-			cout << "Введите число №" << i+1 << ":\n: ";
-			cin >> temp_num;
-			temp.push_back(temp_num);
-			cout << endl;
-		}
+    for (int i = 0; i < temp_counter; i++) {
+      cout << "Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ в„–" << i + 1 << ":\n: ";
+      cin >> temp_num;
+      temp.push_back(temp_num);
+      cout << endl;
+    }
 
-		Tree_Level_Append();
-		tree[0] = temp[0];
+    Tree_Level_Append();
+    tree[0] = temp[0];
 
-		for (int i = 1; i < temp.size(); i++) {
-			Num_Sort(temp[i], 0);
-		}
-	}
-	//	Метод, отвечающий за конвертацию дерево в строку
-	string Tree_ToString() {
-		string output_string = "";
+    for (int i = 1; i < temp.size(); i++) {
+      Num_Sort(temp[i], 0);
+    }
+  }
+  //	РњРµС‚РѕРґ, РѕС‚РІРµС‡Р°СЋС‰РёР№ Р·Р° РєРѕРЅРІРµСЂС‚Р°С†РёСЋ РґРµСЂРµРІРѕ РІ СЃС‚СЂРѕРєСѓ
+  string Tree_ToString() {
+    string output_string = "";
 
-		int zapolnitel_counter = 0, item_count = 0, current_level_num_count = 0;
+    int zapolnitel_counter = 0, item_count = 0, current_level_num_count = 0;
 
-		for (int i = 0; i < level_counter; i++) {
-			zapolnitel_counter = pow(2, level_counter - i) - 1;
-			current_level_num_count = pow(2, i);
+    for (int i = 0; i < level_counter; i++) {
+      zapolnitel_counter = pow(2, level_counter - i) - 1;
+      current_level_num_count = pow(2, i);
 
-			for (int j = 0; j < current_level_num_count; j++) {
-				for (int k = 0; k < zapolnitel_counter; k++) {
-					output_string += " ";
-				}
+      for (int j = 0; j < current_level_num_count; j++) {
+        for (int k = 0; k < zapolnitel_counter; k++) {
+          output_string += " ";
+        }
 
-				output_string += to_string(tree[item_count]);
+        output_string += to_string(tree[item_count]);
 
-				for (int k = 0; k < zapolnitel_counter; k++) {
-					output_string += " ";
-				}
-				if (j < current_level_num_count - 1) output_string += " ";
-				item_count++;
-			}
+        for (int k = 0; k < zapolnitel_counter; k++) {
+          output_string += " ";
+        }
+        if (j < current_level_num_count - 1) output_string += " ";
+        item_count++;
+      }
 
-			output_string += "\n";
-		}
-		return output_string;
-	}
-	//	Метод, отвечающий за вывод в консоль
-	void Tree_Print() {
-		cout << Tree_ToString();
-	}
-	//	Метод, отвечающий за вывод в файл
-	void Tree_SaveToFile() {
-		
-		ofstream setdata(Out_F);
+      output_string += "\n";
+    }
+    return output_string;
+  }
+  //	РњРµС‚РѕРґ, РѕС‚РІРµС‡Р°СЋС‰РёР№ Р·Р° РІС‹РІРѕРґ РІ РєРѕРЅСЃРѕР»СЊ
+  void Tree_Print() { cout << Tree_ToString(); }
+  //	РњРµС‚РѕРґ, РѕС‚РІРµС‡Р°СЋС‰РёР№ Р·Р° РІС‹РІРѕРґ РІ С„Р°Р№Р»
+  void Tree_SaveToFile() {
+    ofstream setdata(Out_F);
 
-		setdata << Tree_ToString();
+    setdata << Tree_ToString();
 
-		setdata.close();
-	}
+    setdata.close();
+  }
 };
 
 int main() {
-	setlocale(LC_ALL, "Rus");
+  ofstream clearOut(Out_F);
+  clearOut.close();
 
-	ofstream clearOut(Out_F);
-	clearOut.close();
+  // vector<int> Test_mass = {10,9,11,10};
+  // Tree_Sort test(Test_mass);
 
-	//vector<int> Test_mass = {10,9,11,10};
-	//Tree_Sort test(Test_mass);
+  Tree_Sort test;
 
-	Tree_Sort test;
+  test.Tree_Print();
+  test.Tree_SaveToFile();
 
-	test.Tree_Print();
-	test.Tree_SaveToFile();
-
-	return 0;
+  return 0;
 }
